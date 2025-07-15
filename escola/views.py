8918ab75237925a84547fc89056199623eca11b4
Eduardo Borges <eduardobrg06@gmail.com>
2025-07-15 18:53:26 -0300

    feat(escola): cria serializer para alunos e cursos

--- escola/views.py
+++ escola/views.py
@@ -1,11 +1,15 @@
-from django.http import JsonResponse
+from rest_framework import viewsets
+from escola.models import Aluno, Curso
+from escola.serializer import AlunoSerializer, CursoSerializer
 
-def alunos(request):
-    if request.method == 'GET':
-        aluno = {
-            'id': 1,
-            'nome': 'João da Silva',
-            'idade': 10,
-            'email': 'joao@gmail.com'
-        }
-        return JsonResponse(aluno)
+class alunosViewSet(viewsets.ModelViewSet):
+    """"Exibindo todos os alunos e alunas"""
+    queryset = Aluno.objects.all()
+    serializer_class = AlunoSerializer
+    
+class CursosViewSet(viewsets.ModelViewSet):
+    """"Exibindo todos os cursos"""
+    queryset = Curso.objects.all()
+    serializer_class = CursoSerializer
+
+
